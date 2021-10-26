d8c0ac4b69010ec83381c9ee3685d447dbca5c4c
tsdimitrova <tsvetelina.tsvyatkova@gmail.com>
2021-10-26 23:45:32 +0300

    Movies web app initial commit

---
+++ MoviesWebApp/Data/RazorPagesMovieContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesWebApp.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(
+            DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesWebApp.Data.Models.Movie> Movie { get; set; }
+    }
+}
